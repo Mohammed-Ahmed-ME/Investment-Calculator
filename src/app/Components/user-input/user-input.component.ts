d62cc5be87e51bd05586429d2020cf2d5226b735
Mohammed Ahmed <ma4539@icloud.com>
2025-07-28 09:53:23 +0300

    This Is an Angular App Investment Calculator

---
+++ src/app/Components/user-input/user-input.component.ts
@@ -0,0 +1,39 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  Initial_Investment = '';
+  Annual_Investment = '';
+  Return_Rate = '5';
+  Duration = '10';
+@Output() submitData = new EventEmitter<{
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+}>();
+
+onSubmit() {
+  this.submitData.emit({
+    initialInvestment: +this.Initial_Investment,
+    annualInvestment: +this.Annual_Investment,
+    expectedReturn: +this.Return_Rate,
+    duration: +this.Duration
+  })
+}
+  calculateInvestment() {
+    const initial = parseFloat(this.Initial_Investment);
+    const annual = parseFloat(this.Annual_Investment);
+    const rate = parseFloat(this.Return_Rate);
+    const duration = parseInt(this.Duration);
+
+console.log(this.Initial_Investment, annual, rate, duration);
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./Components/header/header.component";
+import { UserInputComponent } from "./Components/user-input/user-input.component";
+import {InvestmentResultsComponent} from "./Components/investment-results/investment-results.component";
+import {CurrencyPipe, DecimalPipe} from "@angular/common";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent, CurrencyPipe, DecimalPipe],
+})
+export class AppComponent {
+  submitted :any
+  InvestmentResults: any;
+  getSubmitted(data:any) {
+    this.submitted = data;
+    this.calculateInvestmentResults();
+  }
+   calculateInvestmentResults() {
+    const annualData = [];
+    let investmentValue = this.submitted.initialInvestment;
+
+    for (let i = 0; i < this.submitted.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (this.submitted.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + this.submitted.annualInvestment;
+      const totalInterest =
+        investmentValue - this.submitted.annualInvestment * year - this.submitted.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: this.submitted.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: this.submitted.initialInvestment + this.submitted.annualInvestment * year,
+      });
+    }
+    this.InvestmentResults = annualData;
+  }
+
+}
